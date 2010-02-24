a0ca6fe70ed8256cbf5e650d40c1650915e60937
Joseph Harry <findarato@gmail.com>
2010-02-24 17:46:20 +0000

    Aval now parses the marc record.  When converting over to the marc record view it also made the page render LOTS faster, as that view has a lot less data to be sent to the screen.  It also gies me a ton more information than I could have gotten any where else.  You can now pass the marc record number you want and it will parse it out and return it.  It currenly does not take more than one, but that is not too hard to adjust.

--- lapcat/code/aval.php
+++ lapcat/code/aval.php
@@ -1,4 +1,5 @@
 <?Php
+
 function makeCoolXMLStuff($array,$home="main",$cache=false,$isbn=0){
 	$db = db::getInstance();
 	if($cache){ //if the data is stale we need to work with it
@@ -14,22 +15,52 @@ function makeCoolXMLStuff($array,$home="main",$cache=false,$isbn=0){
 			"bookmobile"=>0,
 			"available"=>0,
 			"available-other"=>0,
-			"available-home"=>0
+			"available-home"=>0,
+			"due"=>0,
+			"next-due-ts"=>0,
+			"due-ts-array"=>array(),
+			"holds"=>0,
+			"total-copies"=>0,
+			"full-mark-record"=>"",
+			"marc-record"=>""
+			
 			);
 			foreach ($array as $k=>$v)	{
-				$loc = explode(" ",substr($k,2));
-				foreach ($v as $v_k=>$v_v)	{
-					if(strpos(ucwords($v_v),'AVAILABLE')){
-						$locs[strtolower(trim($loc[0]))]++;
-						if($home == strtolower(trim($loc[0]))){
-							$locs["available-home"]++;	
-						}else{
-							$locs["available-other"]++;
+				if($k=="holds"){
+					$holdTemp = explode(" ",$v[0]);
+					$locs["holds"] = $locs["holds"] + $holdTemp[0];
+				}elseif($k=="marcR"){
+			//		$locs["full-marc-record"]=$v["full"];
+					$locs["marc-record"]=$v["parsed"];
+				}else{
+					$loc = explode(" ",substr($k,2));
+					foreach ($v as $v_k=>$v_v)	{
+						if(strpos(ucwords($v_v),'AVAILABLE')){
+							$locs[strtolower(trim($loc[0]))]++;
+							if($home == strtolower(trim($loc[0]))){
+								$locs["available-home"]++;	
+							}else{
+								$locs["available-other"]++;
+							}
+							$locs["available"]++;
+						}elseif(strpos(ucwords($v_v),'DUE')){
+							$temp = explode(" ",$v_v);
+							$locs["due-ts-array"][]=strtotime($temp[1]);
+							$locs["due"]++;
+							if(strpos(ucwords($v_v),'HOLD') || strpos(ucwords($v_v),'HOLDS')){
+								$specialHolds = intVal($temp[2],10);
+								$locs["holds"] = $locs["holds"] + $specialHolds;
+							}
 						}
-						$locs["available"]++;
 					}
 				}
 			}
+			foreach($locs["due-ts-array"] as $dta){
+				if($dta > $locs["next-due-ts"]){
+					$locs["next-due-ts"] = $dta;		
+				}	
+			}
+			$locs["total-copies"]=($locs["due"]+$locs["available"]);
 		}else{
 			$locs=array('error'=>'Could not parse item record.');
 		}
@@ -46,7 +77,7 @@ function makeCoolXMLStuff($array,$home="main",$cache=false,$isbn=0){
 	return $locs;
 }
 
-function avalByisbn($isbn,$home="main",$cache=false){
+function avalByisbn($isbn,$home="main",$cache=false,$marc=521){
 	$db = db::getInstance();
 	$returnArray = array();
 	if(is_array($isbn)){
@@ -55,7 +86,8 @@ function avalByisbn($isbn,$home="main",$cache=false){
 		if($db->Count_res()>0){//there are results
 			$res = $db->Fetch("assoc_array");
 			foreach($res as $k=>$r){
-				if($r["updatedDiff"]>300){
+				//if($r["updatedDiff"]>3300){
+				if($r["updatedDiff"]>1){
 					$returnArray[$r["isbn"]] = avalByisbn($r["isbn"],$home,true);
 					$returnArray[$r["isbn"]]["cache"]=1;
 				}else{
@@ -81,16 +113,61 @@ function avalByisbn($isbn,$home="main",$cache=false){
 	
 	
 	//now for the actual processing of the website
-	$url = "http://catalog.lapcat.org/search/i".$isbn;
+	$url = "http://catalog.lapcat.org/search~S12?/i".$isbn."/i".$isbn."/1%2C1%2C1%2CE/marc&FF=i".$isbn;
+	//$url = "http://catalog.lapcat.org/search/i".$isbn;
 	$doc = new DOMDocument();
 	@$doc->loadHTMLFile($url);
 	$elements = $doc->getElementsByTagName('tr');
+	$holds = $doc->getElementsByTagName('span');
+	$marcRecord = $doc->getElementsByTagName('pre');
 	$keep = true;
 	$cnt = 0;
 	$ls = false;
 	$a_status = array();
 	$pnv = "";
 	$multi = false;//default is that it is not a multi item record
+
+//parse the marc record
+	if($marc!=0){
+		if (!is_null($marcRecord)) { //you are not parsing the marc record view
+			foreach ($marcRecord as $mr) {
+				$mcr = explode("\n",$mr->nodeValue);
+				$fixedMCR = array();
+				$preIndex = 0;
+				foreach($mcr as $mr2){
+					if(substr($mr2,0,3)=="   "){
+						$fixedMCR[$preIndex] .= substr_replace($mr2, "", 0,7);
+					}else{
+						$fixedMCR[substr($mr2,0,3)] = substr_replace($mr2, "", 0,7);
+						$preIndex = substr($mr2,0,3);
+					}
+					 
+				}
+				if(isset($fixedMCR[$marc])){$a_status["marcR"]["parsed"] = $fixedMCR[$marc];}else{$a_status["marcR"]["parsed"] = "No Entry";}
+				$a_status["marcR"]["full"] = $mr->nodeValue;
+			}
+		}
+	}
+//Parse out the holds	
+	if (!is_null($holds)) {
+		foreach ($holds as $hold) {
+			if($hold->hasAttributes())
+			{
+			    $attributes = $hold->attributes;
+			    if(!is_null($attributes))
+			    {
+			        foreach ($attributes as $index=>$attr)
+			        {
+						if($attr->name == "class" && $attr->value == "bibHolds"){
+							$a_status["holds"][] = trim($hold->nodeValue);
+						}
+			        }
+			    }
+			} 
+		}
+	}
+	
+//parse out the availablity
 	if (!is_null($elements)) {
 		foreach ($elements as $element) {
 		    $nodes = $element->childNodes;
