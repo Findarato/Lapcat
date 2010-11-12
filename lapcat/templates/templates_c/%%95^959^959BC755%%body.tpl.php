6f9f61d47cfcca41346919b3748100bd94a2bb3a
Joseph Harry <findarato@gmail.com>
2010-11-12 21:26:58 +0000

    Adding templates back in.  These are only to get the site back to normal.

---
+++ lapcat/templates/templates_c/%%95^959^959BC755%%body.tpl.php
@@ -0,0 +1,45 @@
+<?php /* Smarty version 2.6.24, created on 2010-10-13 10:39:22
+         compiled from body.tpl */ ?>
+<table cellpadding="0" cellspacing="0" class="color-X-1" style="height:100%; position:absolute; top:0px; width:100%;"><tr><td style="height:auto; vertical-align:top; width:auto;">
+	<div style="height:100%; width:auto;"> 
+		<table cellpadding="0" cellspacing="0" class="image-background" style="height:100%; width:100%;"><tr><td style="height:auto; vertical-align:top; width:auto;">
+			<div class="color-X-4" style="height:46px; width:100%;">
+				<table cellpadding="0" cellspacing="0" style="height:46px; width:100%;">
+					<tr>
+						<td rowspan="2" style="width:220px; vertical-align:top;">
+							<!--// Stage - LAPCAT Logo //-->
+							<div style="float:left; height:40px; margin-left:8px; margin-top:3px; width:67px;">
+								<img class="fake-link shrink-all-on-click" src="/lapcat/images/101-58-1.png" style="height:38px; width:67px;"/>
+							</div>
+							<div style="float:left; height:40px; margin-left:4px; width:120px;">
+								<div class="LAPCAT-image" style="float:left; height:18px; margin-top:6px; width:100px;"></div>
+								<font class="font-I" style="float:left; margin-left:2px; margin-top:6px;">&trade;</font>
+								<br/><font class="font-X" style="float:left; font-size:14px; margin-top:2px;">My Library, Online</font>
+							</div>
+						</td>
+						<td style="vertical-align:top; width:auto;">
+							<!--// La Porte County Public Library System //-->
+							<div style="width:285px; height:19px;"><a class="library-link-1 font-X" onfocus="javascript:this.blur();" style="font-size:14px; margin-left:6px; margin-top:1px;">La Porte County Public Library System</a></div>
+							<div style="width:285px; height:12px; margin-top:3px;">
+								<div style="float:left;"><img class="fake-link shrink-all-on-click" src="http://cdn1.lapcat.org/fugue/icons/arrow-curve-000-left.png" style="padding-left:12px;" /><a class="fake-link font-X shrink-all-on-click" onfocus="javascript:this.blur();" style="font-size:14px; margin-left:2px; vertical-align:top;">Home</a></div>
+								<div id="level-2" style="display:none; float:left;"><img class="fake-link" src="http://cdn1.lapcat.org/fugue/icons/control-000-small.png" style="padding-left:2px;" /><a class="fake-link font-X" id="level-2-text" onfocus="javascript:this.blur();" style="font-size:14px; margin-left:2px; vertical-align:top;"></a></div>
+								<div id="level-3" style="display:none; float:left;"><img class="fake-link" src="http://cdn1.lapcat.org/fugue/icons/control-000-small.png" style="padding-left:2px;" /><a class="fake-link font-X" id="level-3-text" onfocus="javascript:this.blur();" style="font-size:14px; margin-left:2px; vertical-align:top;"></a></div>
+							</div>
+						</td>
+					</tr>
+				</table>
+			</div>
+			<div style="width:100%;">
+				<div id="interface-content-displays" style="vertical-align:top; width:100%;">
+					<div id="destination-content" style="vertical-align:top; float:left;">
+						<?php $_smarty_tpl_vars = $this->_tpl_vars;
+$this->_smarty_include(array('smarty_include_tpl_file' => ($this->_tpl_vars['content']), 'smarty_include_vars' => array()));
+$this->_tpl_vars = $_smarty_tpl_vars;
+unset($_smarty_tpl_vars);
+ ?>
+					</div>
+				</div>
+			</div>
+		</td></tr></table>
+	</div>
+</td></tr></table>
