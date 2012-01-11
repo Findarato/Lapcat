fe7bf4639d9893f3777c4b15191a88e2d65b7e5a
Joseph Harry <findarato@gmail.com>
2012-01-11 20:57:20 +0000

    the events now looks better and no longer has the images.  There was no good way to include them.

--- ajax/rss.php
+++ ajax/rss.php
@@ -9,7 +9,7 @@
 	curl_setopt($session, CURLOPT_HEADER, false); 	       // Don't return HTTP headers
 	curl_setopt($session, CURLOPT_RETURNTRANSFER, true);   // Do return the contents of the call
 	$xml = curl_exec($session); 	                       // Make the call
-	header("Content-Type: text/xml"); 	               // Set the content type appropriately
+	header("Content-Type: text/xml");                      // Set the content type appropriately
 	echo $xml; 	      // Spit out the xml
 	curl_close($session); // And close the session
 ?>
