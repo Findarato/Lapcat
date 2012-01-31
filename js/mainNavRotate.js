f61aa7878205c1f3d6d015687a596f71608cd55b
Joseph Harry <findarato@gmail.com>
2012-01-31 16:36:18 +0000

    changing the main nav rotate to be a js file and actually run javascript over it

---
+++ js/mainNavRotate.js
@@ -0,0 +1,12 @@
+/*
+ * This will contain the rotating information for the main navagation middle area
+ * 
+ */
+
+ var mainNavagationRotate = {
+ 	"config":{"time":30},
+ 	"data":[
+ 		{"image":"http://laportelibrary.org/images/geek/week03.png","link":"#","title":"this is the title of the link"},
+ 		{"image":"http://laportelibrary.org/images/geek/week04.png","link":"#","title":"this is the title of the link 2"}
+ 	]
+ };
