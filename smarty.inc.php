1935ba43944552c43d657bfa32c22c40ca6d7012
Joseph Harry <findarato@gmail.com>
2012-02-09 16:20:24 +0000

    organizing all of the templates.  This is the first site I have ever done with this many templates.  But with this organization scheme its WAY easier to deal with them.

--- smarty.inc.php
+++ smarty.inc.php
@@ -1,11 +1,11 @@
 <?
 date_default_timezone_set( 'America/Chicago' );
 
-define('SMARTY_DIR', '/www/smarty/libs/');
+define('SMARTY_DIR', $_SERVER["DOCUMENT_ROOT"].'/Smarty/libs/');
 require_once(SMARTY_DIR . 'Smarty.class.php');
 $smarty = new Smarty();
-$smarty->template_dir = '/www/dev/templates'; 
-$smarty->compile_dir = '/www/dev/templates_c';
-$smarty->cache_dir = '/www/dev/cache';
-$smarty->config_dir = '/www/dev/configs';
+$smarty->template_dir = $_SERVER["DOCUMENT_ROOT"].'/templates'; 
+$smarty->compile_dir = $_SERVER["DOCUMENT_ROOT"].'/templates_c';
+$smarty->cache_dir = $_SERVER["DOCUMENT_ROOT"].'/cache';
+$smarty->config_dir = $_SERVER["DOCUMENT_ROOT"].'/configs';
 ?>
