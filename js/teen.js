9bc7a6c7a399450393986672d5b0ea7fa19d66cb
Joseph Harry <findarato@gmail.com>
2012-02-10 16:34:53 +0000

    switched to a jquery based countdown that works with IE

--- js/teen.js
+++ js/teen.js
@@ -1,43 +1,10 @@
-function loadFlicker( queryString ){
-	
-	$('#flickerPictureBox').jflickrfeed({
-		limit: 30,
-		qstrings: {
-			id: queryString
-		},
-		itemTemplate: '<li><a href="{{image_b}}"><img src="{{image_s}}" alt="{{title}}" /></a></li>'
-	});
-/*
-	$('#flickerScrollBox').jflickrfeed({
-		limit: 15,
-		qstrings: {
-			id: queryString
-		},
-		itemTemplate: '<li><a href="{{image_b}}"><img src="{{image}}" alt="{{title}}" /></a><div class="flickrImageTitle">{{title}}</div></li>'
-	}, function(data) {
-		$('#flickerScrollBox div').hide();
-		$('#flickerScrollBox').cycle({
-			timeout: 5000
-		});
-		$('#flickerScrollBox li').hover(function(){
-			$(this).children('div').show();
-		},function(){
-			$(this).children('div').hide();
-		});
-	});
-	/*
-	$('#nocallback').jflickrfeed({
-		limit: 4,
-		qstrings: {
-			id: queryString
-		},
-		useTemplate: false,
-		itemCallback: function(item){
-			$(this).append("<li><img src='" + item.image_m + "' alt=''/></li>");
+//loadFlicker("62092835@N08");
+$("#cntDwn")
+	.countdown(
+		{
+			"until": new Date(2012,3,23), 
+			"format":'yODHMS',
+			"layout": "<div class='countDownDays'>{dn} {dl}</div><div class='countDownHours'>{hn} {hl}</div><div class='countDownMinutes'>{mn} {ml}</div><div class='countDownSeconds'>{sn} {sl}</div>"
 		}
-	});
-*/
-}
-
-
-loadFlicker("62092835@N08"); 
+	);
+	
