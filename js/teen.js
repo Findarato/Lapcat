3b3822aab89446f1d98f0a015a135417ce5441d2
Joseph Harry <findarato@gmail.com>
2012-03-23 14:04:24 +0000

    started on the children page.
    Changed the countdown to be for the avengers movie

--- js/teen.js
+++ js/teen.js
@@ -2,9 +2,9 @@
 $("#cntDwn")
 	.countdown(
 		{
-			"until": new Date(2012,3,23), 
+			"until": new Date(2012,05,04),  
 			"format":'yODHMS',
-			"layout": "<div class='countDownDays'>{dn} {dl}</div><div class='countDownHours'>{hn} {hl}</div><div class='countDownMinutes'>{mn} {ml}</div><div class='countDownSeconds'>{sn} {sl}</div>"
+			"layout": "<div class='countDownContainer'><div class='countDownDays'>{dn} {dl}</div><div class='countDownHours'>{hn} {hl}</div><div class='countDownMinutes'>{mn} {ml}</div><div class='countDownSeconds'>{sn} {sl}</div></div>"
 		}
 	);
 	
