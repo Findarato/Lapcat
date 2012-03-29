6468b535a2b79f1f720a1d405f0d2cc0ce72fd6c
Joseph Harry <findarato@gmail.com>
2012-03-29 15:18:46 +0000

    moved the flicker display into a function inside of scripts and just called it in kids and teens.
    Those two javascirpt files are starting to just be copies of each other with very small adjustments.  Guess 2 really tiny js files will be required but atlest they are very small.

--- js/script.js
+++ js/script.js
@@ -496,7 +496,37 @@ function get_blog_feed() {
 	});
 	
 };
-
+function displayFlickr(flickrTag,flickrId){
+	// lets make sure we have some defaults in so that if the user just calls the function it works
+	if(flickrTag == undefined)
+		flickrTag = "teen";
+	if(flickrId == undefined)
+		flickrId = "62092835@N08";
+		
+	$.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
+	{
+		id: flickrId,
+    	format: "json",
+    	tags:flickrTag
+	},
+	function(data) {
+	  	$.each(data.items, function(i,item){
+	    	$("<div/>",{"class":"flickrPicture linearAnimate",css:{"display":"inline-block","position":"relative"}})
+	      		.html(
+	      			$("<div/>",{"class":"flickrPictureOutline roundAll3 linearAnimate"})
+	      				.html(
+		      				$("<a/>",{"html":"&nbsp;"})
+	    	  					.css({"background-image":"url("+item.media.m+")"})
+	      						.attr({"href":item.link,"title":item.title})	
+	      				)
+	      				.append(
+	      					$("<div/>",{"html":item.title,"css":{"position":"relative","top":"106px","left":"3px","width":"96%","font-size":"1em","color":"#000","margin-left:":"2%"}})		
+	      				)
+	      		)
+	      		.appendTo("#flickerPictureBox");
+	    });
+	});
+}
 $(document).ready(function(){
 		if($("#blogBox")){get_blog_feed();}
 		uri = window.location.toString();
