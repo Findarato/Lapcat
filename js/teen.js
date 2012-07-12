8ac4afe07bf4c2cc677eef034be625a8b240dee1
Joseph Harry <findarato@gmail.com>
2012-07-12 15:35:41 +0000

    Working extensively on the downloads page.  Need to wait till the live site gets back so I can see what I am missing.

--- js/teen.js
+++ js/teen.js
@@ -1,13 +1,4 @@
 //loadFlicker("62092835@N08");
-$("#cntDwn")
-	.countdown(
-		{
-			"until": new Date(2012,05,04),  
-			"format":'yODHMS',
-			"layout": "<div class='countDownContainer'><div class='countDownDays'>{dn} {dl}</div><div class='countDownHours'>{hn} {hl}</div><div class='countDownMinutes'>{mn} {ml}</div><div class='countDownSeconds'>{sn} {sl}</div></div>"
-		}
-	);
-displayFlickr("teens",'62092835@N08');
 $(window).load(function(){
 	wowbraryImageLinks("http://www.wowbrary.org/rss.aspx?l=8711&c=TEE",$("#topBooks").find(".slides"),true,"Newest Teen Books",10); 
 });
