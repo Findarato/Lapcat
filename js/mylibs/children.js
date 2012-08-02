5c90b8e17c3c242328dd35aba990ab8ef378dba8
Joseph Harry <findarato@gmail.com>
2012-08-02 17:30:08 +0000

    the teen page works correctly again.  Tweeked a bit of the css to make the hovers work better.

--- js/mylibs/children.js
+++ js/mylibs/children.js
@@ -1,13 +1,5 @@
-//loadFlicker("62092835@N08");
-$("#cntDwn")
-	.countdown(
-		{
-			"until": new Date(2012,05,04),  
-			"format":'yODHMS',
-			"layout": "<div class='countDownContainer'><div class='countDownDays'>{dn} {dl}</div><div class='countDownHours'>{hn} {hl}</div><div class='countDownMinutes'>{mn} {ml}</div><div class='countDownSeconds'>{sn} {sl}</div></div>"
-		}
-	);
-displayFlickr("children",'62092835@N08');
+
 $(window).load(function(){
+  displayFlickr("children",'62092835@N08');
 	wowbraryImageLinks("http://www.wowbrary.org/rss.aspx?l=8711&c=CHI",$("#topBooks").find(".slides"),true,"Newest Kids Books",10); 
 });
