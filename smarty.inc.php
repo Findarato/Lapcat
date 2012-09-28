5e486bb1c9b12dc30795b680b4c15ed4c168e729
Joseph Harry <findarato@gmail.com>
2012-09-28 12:03:42 -0500

    changed the Php tag to work with the <?Php

--- smarty.inc.php
+++ smarty.inc.php
@@ -1,11 +1,12 @@
-<?
+<?Php
 date_default_timezone_set( 'America/Chicago' );
 
 define('SMARTY_DIR', $_SERVER["DOCUMENT_ROOT"].'/Smarty/libs/');
+
+echo SMARTY_DIR;
 require_once(SMARTY_DIR . 'Smarty.class.php');
 $smarty = new Smarty();
 $smarty->template_dir = $_SERVER["DOCUMENT_ROOT"].'/templates'; 
 $smarty->compile_dir = $_SERVER["DOCUMENT_ROOT"].'/templates_c';
 $smarty->cache_dir = $_SERVER["DOCUMENT_ROOT"].'/cache';
-$smarty->config_dir = $_SERVER["DOCUMENT_ROOT"].'/configs';
-?>
+$smarty->config_dir = $_SERVER["DOCUMENT_ROOT"].'/configs';  
