8621085fe9ae95dad6b5302f00d0e6c02d37c9ea
Joseph Harry <findarato@gmail.com>
2012-11-01 14:21:06 -0500

    getting the node template

--- drupal/sites/all/themes/lapcat/node.tpl.php
+++ drupal/sites/all/themes/lapcat/node.tpl.php
@@ -1,13 +1,14 @@
-<div class="box blogBox roundAll3">
-  <div class="blogTitle roundAll3 titleElement color3 icon-vkontakte-rect font1">
-    <?php if ($title): ?><?php print $title ?><?php endif; ?>
-  </div>
-  <div id="blogContainerBox" class="insideBoxShadow roundAll3 containerBox" >
-      <div id="" class="clearfix">
-        <?php print $content ?>
+<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
+  <div class="box blogBox roundAll3">
+    <div class="blogTitle roundAll3 titleElement color3 icon-vkontakte-rect font1">
+      <?php if ($title): ?><?php print $title ?><?php endif; ?>
     </div>
+    <div id="blogContainerBox" class="insideBoxShadow roundAll3 containerBox" >
+        <div id="" class="clearfix">
+          <?php print $content ?>
+      </div>
+  </div>
 </div>
-
 <?php
 ?>
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
