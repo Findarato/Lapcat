1dcb13455c51f7b4a3a37870daec53b9a09e47f8
Joseph Harry <findarato@gmail.com>
2012-11-02 16:09:29 -0500

    Adding in the modules I am using to the repo.

--- drupal/sites/all/themes/lapcat/node.tpl.php
+++ drupal/sites/all/themes/lapcat/node.tpl.php
@@ -1,17 +1,4 @@
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
-  <div class="box blogBox roundAll3">
-    <div class="blogTitle roundAll3 titleElement color3 icon-vkontakte-rect font1">
-      <?php if ($title): ?><?php print $title ?><?php endif; ?>
-    </div>
-    <div id="blogContainerBox" class="insideBoxShadow roundAll3 containerBox" >
-        <div id="" class="clearfix">
-          <?php print $content ?>
-      </div>
-  </div>
-</div>
-<?php
-?>
-<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
 
   <?php print $user_picture; ?>
 
