8946c8568dc6ed7fb06fc2850c70b57e16bcb101
Joseph Harry <findarato@gmail.com>
2013-02-28 10:03:40 -0600

    updated the min-height of the body to be 600px this fixes a lot of the issues with the footer not being in the right spot, and it still looks fine.

--- sites/all/themes/lapcat/js/global.js
+++ sites/all/themes/lapcat/js/global.js
@@ -99,15 +99,6 @@ $(document).ready(function(){
     if(uri.search(/research/i)>0){// this is the research page
       $.getScript("/sites/all/themes/lapcat/js/research.js", function(data, textStatus, jqxhr) {
       });
-      $(".linkIcon a[href^='http'] ").each(function() {
-          $(this)
-          .css({
-              background: "url(http://g.etfv.co/" + this.href + ") left center no-repeat",
-              "padding-left": "20px",
-              "background-size":"16px 16px"
-          });
-      });
-
     }else{
       if(uri.search(/teens/i)>0){// this is the teens page 
         displayFlickr("teens");
@@ -124,6 +115,19 @@ $(document).ready(function(){
         } 
       }
     }
+    
+    //Getting fav icons for all the links that have the class linkIcon
+  $(".linkIcon a[href^='http'] ").each(function() {
+      $(this)
+      .css({
+          background: "url(http://g.etfv.co/" + this.href + ") left center no-repeat",
+          "padding-left": "20px",
+          "background-size":"16px 16px"
+      });
+  });
+
+
+
 });
 
 
