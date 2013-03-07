9cf5c8ea1f474ebcdf27e05c7c0351eba76bba0a
Joseph Harry <findarato@gmail.com>
2013-03-07 11:51:38 -0600

    adding in a module and reverting the version of jquery

--- sites/all/themes/lapcat/templates/html.tpl.php
+++ sites/all/themes/lapcat/templates/html.tpl.php
@@ -43,19 +43,17 @@
  * @ingroup themeable
  */
 ?><!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>
-
+<html>
 <head profile="<?php print $grddl_profile; ?>">
-  
   <meta name="viewport" content="width=device-width" />
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
-  <?php print $scripts; ?>
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
   <?php print $page_top; ?>
   <?php print $page; ?>
   <?php print $page_bottom; ?>
+  <?php print $scripts; ?>
 </body>
 </html>
