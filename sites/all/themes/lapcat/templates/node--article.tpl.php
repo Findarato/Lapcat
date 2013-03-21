9556d0a555ca5796e49dc9c49a8f45afc9941d11
Joseph Harry <findarato@gmail.com>
2013-03-21 09:49:07 -0500

    Updating versions of some modules

--- sites/all/themes/lapcat/templates/node--article.tpl.php
+++ sites/all/themes/lapcat/templates/node--article.tpl.php
@@ -1,6 +1,6 @@
       <style>
         .userImage img{
-          width:64px;
+          width:ssh px;
           height:64px;
           margin:0px;
           padding:0px;
@@ -9,33 +9,36 @@
           padding:3px;
         }
       </style>
-      
-      <div id="node-<?php print $node -> nid; ?>" class="blogEntry roundAll3 <?php print $classes; ?>"<?php print $attributes; ?>>
-        <div class="t">
-          <div class="td" style="height:64px;width:64px;">
-            <div class="userImage" style="width:100%;height:100%;border:none;"><?php print $user_picture; ?></div>
-          </div>
-          <div class="td">
-            <div class="blogEntryTitle"><a href="<?php print $node_url; ?>"><?php print $title; ?></a></div>
-            <div>        
-              <?php if ($display_submitted): ?>
-              <span class="blogEntryAuthor"><?php print $submitted ?></span>
-              <?php endif; ?>
-           </div>
-          </div>
-        </div>
-        <div class="clearfix">
-          <?php if (!empty($content['links'])): ?>
-          <div class="content clearfix"<?php print $content_attributes; ?>>
-            <?php
-              // We hide the comments and links now so that we can render them later.
-              hide($content['comments']);
-              hide($content['links']);
-              print render($content);
-            ?>
-          </div>
-            <div class="links"><?php print render($content['links']); ?></div>
-          <?php endif; ?>
-          <?php print render($content['comments']); ?>
-        </div>
+<div class="box blogBox roundAll3 <?php print $classes; ?>" id="node-<?php print $node -> nid; ?>" <?php print $attributes; ?>>
+  <div class="roundAll3 titleElement color3 font1"><span style="padding-left:3px;height:100%;width:100%">Article</span></div>
+  <div id="blogContainerBox" class="insideBoxShadow roundAll3 containerBox" >
+    <div class="t">
+      <div class="td" style="height:64px;width:64px;">
+        <div class="userImage" style="width:100%;height:100%;border:none;"><?php print $user_picture; ?></div>
       </div>
+      <div class="td">
+        <div class="blogEntryTitle"><a href="<?php print $node_url; ?>"><?php print $title; ?></a></div>
+        <div>        
+          <?php if ($display_submitted): ?>
+          <span class="blogEntryAuthor"><?php print $submitted ?></span>
+          <?php endif; ?>
+       </div>
+      </div>
+    </div> 
+  <!-- Comments -->
+    <div class="clearfix">
+      <?php if (!empty($content['links'])): ?>
+      <div class="content clearfix"<?php print $content_attributes; ?>>
+        <?php
+          // We hide the comments and links now so that we can render them later.
+          hide($content['comments']);
+          hide($content['links']);
+          print render($content);
+        ?>
+      </div>
+        <div class="links"><?php print render($content['links']); ?></div>
+      <?php endif; ?>
+      <?php print render($content['comments']); ?>
+    </div>
+  </div>
+</div>
