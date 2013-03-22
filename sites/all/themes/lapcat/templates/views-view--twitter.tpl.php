c7cc314b2f28cc54966d78542ee7fac2b0c662da
Joseph Harry <findarato@gmail.com>
2013-03-22 11:04:39 -0500

    reduced the number of templates using the template suggestion module.  Got twitter working agian

---
+++ sites/all/themes/lapcat/templates/views-view--twitter.tpl.php
@@ -0,0 +1,25 @@
+<div class="box blogBox roundAll3">
+  <div class="boxTitle roundAll3 titleElement color3">
+    <a href="http://twitter.com/lpcpls">Twitter</a>
+  </div>
+  <div id="blogContainerBox" class="insideBoxShadow roundAll3 containerBox" style="padding-left:10px;">
+    <?php if ($rows): ?>
+      <div class="view-content <?php print $classes; ?>">
+        <?php print $rows; ?>
+      </div>
+    <?php elseif ($empty): ?>
+      <div class="view-empty">
+        <?php print $empty; ?>
+      </div>
+    <?php endif; ?>
+  <?php if ($pager): ?>
+    <?php print $pager; ?>
+  <?php endif; ?>    
+  <?php if ($more): ?>
+    <?php print $more; ?>
+  <?php endif; ?>
+  </div> 
+</div>
+
+
+   
