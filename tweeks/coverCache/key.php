d608ae56370fc9681cbc8dd042f293e0e1392177
Joseph Harry <findarato@gmail.com>
2013-08-14 09:25:22 -0500

    Delete key.php

--- tweeks/coverCache/key.php
+++
@@ -1,5 +0,0 @@
-<?Php
-  $service_url = "http://ecs.amazonaws.com/onca/xml?Service=AWSECommerceService";
-  $associate_tag = "your-associate-tag";
-  $secret_key = "oMFTRGZ7kIizLdG/L0mPPMXD+8UYsZArcInDdPFK";
-  $access_key = "AKIAJ5B7OUJ5VJZ5473Q";
