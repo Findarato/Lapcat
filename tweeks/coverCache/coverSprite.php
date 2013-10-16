c7353ccb3f14b7d8260df1df50f0fa920ad339d2
Joseph Harry <findarato@gmail.com>
2013-10-16 02:58:12 -0500

    Adding in the start of the new cover sprite. This will be used for the cover display on the full javascript version of the slider.

---
+++ tweeks/coverCache/coverSprite.php
@@ -0,0 +1,14 @@
+<?Php
+/* Started October 16, 2013 
+ * Should take a string of ISBN and create an image sprite of book covers, The sprite will have assumed image dementions 
+ * and spacing horizontally and vertially. This will be the only way to allow for the generation of the CSS, which will not be
+ * done on by this PHP file, but asumed by the CSS (LESS/SCSS).
+ * 
+ */
+$vSpacing = 5;
+$hSpacing = 5;
+$totalImage = 0;
+
+
+
+?>
