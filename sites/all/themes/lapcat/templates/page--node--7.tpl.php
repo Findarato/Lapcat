6744bd1bedd471cc5e981ebcb744d72fc9051d29
Joseph Harry <findarato@gmail.com>
2013-11-13 15:28:27 -0600

    Fix some of the spacing issues and the kids page

--- sites/all/themes/lapcat/templates/page--node--7.tpl.php
+++ sites/all/themes/lapcat/templates/page--node--7.tpl.php
@@ -2,34 +2,9 @@
 
 <style>
 
-.subPageHeader{ 
-  background:rgba(250,250,250,.6);
-  background-image: url('<?php print $base_path . $directory .'/images/kidsHeader.png'; ?>');  
-}
  .childrenLinks ul li{display:inline-block;}
  .mainArea{z-index: 10;position:relative;}
  /*sprite Code*/
-.sprite{background-image:url("<?php print $base_path . $directory .'/images/csg-511916e61907f.png'; ?>"); display:block;text-indent:100%;overflow:hidden;white-space:nowrap;}
-.sprite-Discovery_Kids_logo{ background-position: 0 0; width: 100px; height: 100px; } 
-.sprite-Disneydigitalbooks{ background-position: 0 -105px; width: 100px; height: 100px; } 
-.sprite-Lego{ background-position: 0 -210px; width: 100px; height: 100px; } 
-.sprite-NGLittleKids{ background-position: 0 -315px; width: 100px; height: 100px; } 
-.sprite-NickJrLogo{ background-position: 0 -420px; width: 100px; height: 100px; } 
-.sprite-PBS_Kids{ background-position: 0 -525px; width: 100px; height: 100px; } 
-.sprite-Starfall{ background-position: 0 -630px; width: 100px; height: 100px; } 
-.sprite-TumbleBook{ background-position: 0 -735px; width: 100px; height: 98px; } 
-.sprite-TumbleBookCloud{ background-position: 0 -838px; width: 100px; height: 98px; } 
-.sprite-TumbleBookCloudjr{ background-position: 0 -941px; width: 100px; height: 98px; } 
-.sprite-TumbleBook_icon{ background-position: 0 -1044px; width: 100px; height: 100px; } 
-.sprite-Yahookids{ background-position: 0 -1149px; width: 100px; height: 100px; } 
-.sprite-barbie{ background-position: 0 -1254px; width: 100px; height: 100px; } 
-.sprite-cartoon_network{ background-position: 0 -1359px; width: 100px; height: 100px; } 
-.sprite-chugginton{ background-position: 0 -1464px; width: 100px; height: 100px; } 
-.sprite-coloring_com{ background-position: 0 -1569px; width: 100px; height: 100px; } 
-.sprite-disneygames{ background-position: 0 -1674px; width: 104px; height: 104px; } 
-.sprite-funbrain{ background-position: 0 -1783px; width: 100px; height: 100px; } 
-.sprite-sesamestreet{ background-position: 0 -1888px; width: 100px; height: 100px; } 
-.sprite-youtube{ background-position: 0 -1993px; width: 115px; height: 50px; } 
 </style>
 <header class="hd-top color1" style="width:100%;">
   <nav class=" container_24">
